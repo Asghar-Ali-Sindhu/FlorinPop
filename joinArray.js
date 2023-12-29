0e37306d150878beb32c7cdaedd9c8bc3f033ccc
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-29 15:16:47 +0500

    joinArray is added

---
+++ joinArray.js
@@ -0,0 +1,22 @@
+// todo: The join() method returns an array as a string. The join() method does not change the original array. Any separator can be specified. The default is comma (,).
+// ! Syntax: array.join(separator)
+// * Example:
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let text = fruits.join('');
+// console.log(text);
+// * Example: Another separator
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let text = fruits.join(" and ");
+// console.log(text);
+// * Example:
+// const elements = ['Fire', 'Air', 'Water'];
+// console.log(elements.join()); //"Fire,Air,Water"
+// * Example:
+// const elements = ['Fire', 'Air', 'Water'];
+// console.log(elements.join('')); // FireAirWater
+// * Example:
+// const elements = ['Fire', 'Air', 'Water'];
+// console.log(elements.join('-')); // Fire-Air-Water
+// * Example: Comma and space
+// const elements = ['Fire', 'Air', 'Water'];
+// console.log(elements.join(', ')); // Fire, Air, Water
