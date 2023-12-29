2c13967367bf8a29d7e630083074ead864d0b326
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-29 15:05:20 +0500

    isArray is added

---
+++ isArray.js
@@ -0,0 +1,27 @@
+// todo: The isArray() method returns true if an object is an array, otherwise false.
+// ! Syntax: Array.isArray(obj)
+// *** Example: Check if an object is an array:
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let result = Array.isArray(fruits);
+// console.log(result); // true
+// *** Example:Check if another datatype is an array:
+// let text = "W3Schools";
+// let result = Array.isArray(text);
+// console.log(result);  // false
+// *** Example:
+// console.log(Array.isArray([1, 3, 5])); // true
+// *** Example:
+// console.log(Array.isArray('[]')); // false
+// *** Example:
+// console.log(Array.isArray(new Array(5))); // true
+// *** Example:
+// console.log(Array.isArray(new Int16Array([15, 33]))); //false
+
+// ! Example:
+// const names = ['Haris', 'Danish', 'Zain'];
+// const str = 'Hello word';
+// const number = 17;
+// console.log(Array.isArray(names)); //true
+// console.log(Array.isArray(str)); //false
+// console.log(Array.isArray(number)); //false
+
