ec5edc4d31a601dfbe82ea0822a7f7abe59a2cd9
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-29 16:15:15 +0500

    popArray is added

---
+++ popArray.js
@@ -0,0 +1,33 @@
+// todo: The pop() method removes (pops) the last element of an array. The pop() method changes the original array. The pop() method returns the removed element.
+// !Syntax: array.pop()
+// * Example: Remove (pop) the last element:
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.pop();
+// console.log(fruits); // "Mango is removed from the array."
+
+// * Example: Pop() returns the removed element.
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.pop());
+// * Example:
+// const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+// console.log(plants.pop()); // Expected output: "tomato"
+// * Example:
+// const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+// console.log(plants);
+// // Expected output: Array ["broccoli", "cauliflower", "cabbage", "kale"]
+// plants.pop();
+// console.log(plants);
+// Expected output: Array ["broccoli", "cauliflower", "cabbage"]
+// * Example: We can always remove the last element as many times as the length of array is
+// const numbers = [1, 2, 3]
+// console.log(numbers);
+// numbers.pop()
+// console.log(numbers);
+// numbers.pop()
+// console.log(numbers);
+// numbers.pop()
+// console.log(numbers);
+// * Example: We can also get the last element of an array
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(numbers.pop());
+// * Example:
